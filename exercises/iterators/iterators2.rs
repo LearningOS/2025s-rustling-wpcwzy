fa58758fb1f524ea311a8cffdc1a08dd8f5c16c1
Wang Pincheng <wpcwzy@outlook.com>
2025-03-31 13:31:00 +0800

    structs 部分iterators和入门 2025-03-11

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -15,16 +15,20 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut ret  = first.to_uppercase().to_string();
+            ret.push_str(c.as_str());
+            ret
+        },
     }
 }
 
-// Step 2.
+// Step 
 // Apply the `capitalize_first` function to a slice of string slices.
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -32,7 +36,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut result = String::new();
+    words.iter().for_each(|&word| {
+        result.push_str(&capitalize_first(word));
+    });
+    result
 }
 
 #[cfg(test)]
